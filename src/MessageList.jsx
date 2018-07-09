a50ac8be7ddabf369d325ee74ee139348f5fd060
Theresa Campbell <t.campbell@sasktel.net>
2018-07-09 23:43:38 +0000

    Broke initial HTML into functional components and put css into home.scss

---
+++ src/MessageList.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Message } from "./Message.jsx";
+
+export function MessageList() {
+  return (
+    <main className="messages">
+      <Message/>
+    </main>
+  )
+}
