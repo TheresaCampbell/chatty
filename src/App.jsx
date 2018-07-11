157a23616753a4ef24b7e3f77fdb2c5047a3a423
Theresa Campbell <t.campbell@sasktel.net>
2018-07-11 17:04:19 +0000

    Connected to WebSocket server. When the user types a message and hite Enter, the server receives it.

--- src/App.jsx
+++ src/App.jsx
@@ -38,24 +38,33 @@ class App extends Component {
       username: this.state.currentUser.name,
       content: content
     }
-    const oldMessages = this.state.messages;
-    const newMessages = [...oldMessages, message];
-    this.setState({messages: newMessages});
+    this.webSocket.send(`User ${message.username} said ${message.content}`);
+    // const oldMessages = this.state.messages;
+    // const newMessages = [...oldMessages, message];
+    // this.setState({messages: newMessages});
   }
 
 
-  // componentDidMount() {
-  // console.log("componentDidMount <App />");
-  // setTimeout(() => {
-  //   console.log("Simulating incoming message");
-  //   // Add a new message to the list of messages in the data store
-  //   const newMessage = {id: 3, username: "Michelle", content: "Hello there!"};
-  //   const messages = this.state.messages.concat(newMessage)
-  //   // Update the state of the app component.
-  //   // Calling setState will trigger a call to render() in App and all child components.
-  //   this.setState({messages: messages})
-  //   }, 3000);
-  // }
+  componentDidMount() {
+    console.log("componentDidMount <App />");
+
+    setTimeout(() => {
+      console.log("Simulating incoming message");
+      // Add a new message to the list of messages in the data store
+      const newMessage = {id: 3, username: "Michelle", content: "Hello there!"};
+      const messages = this.state.messages.concat(newMessage)
+      // Update the state of the app component.
+      // Calling setState will trigger a call to render() in App and all child components.
+      this.setState({messages: messages})
+      }, 3000);
+
+    this.webSocket = new WebSocket("ws://localhost:3001/");
+
+    this.webSocket.onopen = (event) => {
+      console.log("Connected to server");
+    };
+
+  }
 
   render() {
     console.log("Rendering App");
