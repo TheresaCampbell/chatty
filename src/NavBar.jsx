59ab9104637a67f9dc4680d18ebf3aefdb0c8481
Theresa Campbell <t.campbell@sasktel.net>
2018-07-13 19:58:38 +0000

    Used conditional rendering in NavBar component to display 1 USER online when only one user is connected and X USERS otherwise.

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -2,10 +2,17 @@ import React, {Component} from 'react';
 
 class NavBar extends Component {
   render() {
+    const isSingular = this.props.clients === 1;
+
     return (
       <nav className="navbar">
         <a href="/" className="navbar-brand">Chatty</a>
-        <p className="user-count">{this.props.clients} users online</p>
+
+        {isSingular ? (
+          <p className="user-count">{this.props.clients} user online</p>
+        ) : (
+          <p className="user-count">{this.props.clients} users online</p>
+        )}
       </nav>
     )
   }
